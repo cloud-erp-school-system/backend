c003aa2f05446afb1a2bc759e98acceaf8a026f3
Alisher <arurunov@gmail.com>
2021-03-11 09:55:59 +0200

    #11 Keycloak POC (#12)
    * #11 Keycloak POC

--- src/main/java/org/erp/school/config/SecurityConfiguration.java
+++ src/main/java/org/erp/school/config/SecurityConfiguration.java
@@ -1,84 +1,56 @@
 package org.erp.school.config;
 
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.keycloak.adapters.springsecurity.management.HttpSessionManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
-@EnableWebSecurity
-@Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-  private static final String LOGIN_PROCESSING_URL = "/login";
-  private static final String LOGIN_FAILURE_URL = "/login?error";
-  private static final String LOGIN_URL = "/login";
-  private static final String LOGOUT_SUCCESS_URL = "/login";
+@KeycloakConfiguration
+public class SecurityConfiguration extends KeycloakWebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.csrf()
-        .disable()
-        .authorizeRequests()
-        .anyRequest()
-        .permitAll()
-        .and()
-        .formLogin()
-        .loginPage(LOGIN_URL)
-        .permitAll()
-        .loginProcessingUrl(LOGIN_PROCESSING_URL)
-        .failureUrl(LOGIN_FAILURE_URL)
-        .and()
-        .logout()
-        .logoutSuccessUrl(LOGOUT_SUCCESS_URL)
+    super.configure(http);
+    http.authorizeRequests().anyRequest().authenticated();
+    http.csrf().disable();
+  }
 
-        // Configure logout
-        .and()
-        .logout()
-        .logoutSuccessUrl(LOGOUT_SUCCESS_URL);
+  @Autowired
+  public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) {
+    SimpleAuthorityMapper simpleAuthorityMapper = new SimpleAuthorityMapper();
+    simpleAuthorityMapper.setPrefix("ROLE_");
+
+    KeycloakAuthenticationProvider keycloakAuthenticationProvider =
+        keycloakAuthenticationProvider();
+    keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(simpleAuthorityMapper);
+    authenticationManagerBuilder.authenticationProvider(keycloakAuthenticationProvider);
   }
 
   @Bean
   @Override
-  public UserDetailsService userDetailsService() {
-    UserDetails user = User.withUsername("user").password("{noop}password").roles("USER").build();
-
-    return new InMemoryUserDetailsManager(user);
+  protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+    return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
   }
 
+  @Bean
   @Override
-  public void configure(WebSecurity web) {
-    web.ignoring()
-        .antMatchers(
-            // Client-side JS
-            "/VAADIN/**",
+  @ConditionalOnMissingBean(HttpSessionManager.class)
+  protected HttpSessionManager httpSessionManager() {
+    return new HttpSessionManager();
+  }
 
-            // the standard favicon URI
-            "/favicon.ico",
-
-            // the robots exclusion standard
-            "/robots.txt",
-
-            // web application manifest
-            "/manifest.webmanifest",
-            "/sw.js",
-            "/offline.html",
-
-            // icons and images
-            "/icons/**",
-            "/images/**",
-            "/styles/**",
-
-            // (development mode) H2 debugging console
-            "/h2-console/**",
-
-            // initial api test
-            "/greeting",
-            "/helloController");
+  @Bean
+  public KeycloakSpringBootConfigResolver keycloakSpringBootConfigResolver() {
+    return new KeycloakSpringBootConfigResolver();
   }
 }
