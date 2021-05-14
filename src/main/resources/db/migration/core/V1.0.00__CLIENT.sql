2b0c65f68355122ef41b1518bd6acd52cacb538d
Alisher <arurunov@gmail.com>
2021-05-14 17:41:03 +0300

    Delete V1.0.00__CLIENT.sql

--- src/main/resources/db/migration/core/V1.0.00__CLIENT.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE client
-(
-    id           VARCHAR(50)  NOT NULL PRIMARY KEY,
-    name         VARCHAR(100) NOT NULL,
-    address_id   VARCHAR(50)  NOT NULL,
-    created_date TIMESTAMP
-);
